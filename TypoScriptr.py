41a4ee4e7da5e370710eb29ba0f3a75000a0f7b5
Philipp Kitzberger <philipp@kitze.net>
2014-05-23 16:27:57 +0200

    Test

--- TypoScriptr.py
+++ TypoScriptr.py
@@ -6,21 +6,38 @@ class TyposcriptrCommand(sublime_plugin.TextCommand):
 			if region.empty():
 				# Beginning and end of this line, e.g.: (0, 30)
 				line = self.view.line(region)
+				posLineBegin = line.a
+				posLineEnd = line.b
 				string = self.view.substr(line)
-				print ("TypoScriptr processing this string: '" + string + "'")
+				#print ("TypoScriptr processing this string: '" + string + "'")
 
-				# Example: "page.10.template.value = hello world"
-				matchObj = re.match(r'.*\s*(=|<|=<|>)\s*[^\s]*\s*$', string, re.M)
-				if (matchObj):
+				# Text after cursor
+				posCursor = self.view.sel()[0].begin()
+				range = sublime.Region(posCursor, line.b)
+				textAfterCursor = self.view.substr(range).strip()
+
+				# Split at operator (only once -> third param!)
+				stringSplitted = re.split(r'(=|<|=<|>)', string, 1)
+				lengthOfTextInfrontOfCursor = len(stringSplitted[0])
+				posOperator = posLineBegin + lengthOfTextInfrontOfCursor
+				#print ("Splitted string: " + str(stringSplitted))
+				#print ("posLineBegin: " + str(posLineBegin) + ', posCursor: ' + str(posCursor) + ', lengthOfTextBeforeCursor: ' + str(lengthOfTextInfrontOfCursor) + ', posOperator: ' + str(posOperator))
+
+				# Was cursor set behind operator?
+				if (posCursor > posOperator):
+					#print ("Cursor is after operator")
+
+					# Example: "page.10.template.value = hello world"
 					newLine = re.split(r'(=|<|=<|>)', string)
 					newLine = '\n' + newLine[0].rstrip()
 
 					# Insert: "page.10.template.value"
-					self.view.insert(edit, line.end(), newLine)
+					self.view.insert(edit, posCursor, newLine)
 				else:
+					#print ("Cursor was before operator a.k.a. no operator detected!")
 					matchObj = re.match(r'(\s*)(\w*).*$', string, re.M)
 					whitespaces = matchObj.group(1)
-					#print ("Whitespace found: '" + whitespaces + "'")
+					#print ("Leading whitespaces found: '" + whitespaces + "'")
 					newLines = ' {\n'+whitespaces+'\t\n'+whitespaces+'}'
 
 					# Append: " {LINEBREAK TABS TAB LINEBREAK TABS}"
