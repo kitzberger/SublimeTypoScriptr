3a58f2180836d8c0220f3901ec2841730ccab6a4
Philipp Kitzberger <philipp@kitze.net>
2016-07-04 02:00:44 +0200

    Handling text to the right of the cursor correctly

--- TypoScriptr.py
+++ TypoScriptr.py
@@ -21,26 +21,30 @@ class TyposcriptrCommand(sublime_plugin.TextCommand):
 					newLine = '\n' + newLine[0].rstrip()
 
 					# Insert: "page.10.template.value"
-					self.view.insert(edit, line.end(), newLine)
+					self.view.insert(edit, region.end(), newLine)
 				else:
 					if region.end() == line.end():
 						print ("* Case 2a: line not containing TypoScript operator and cursor at end of it")
 					else:
 						print ("* Case 2b: line not containing TypoScript operator and cursor within line")
 
+					regionRightToCursor = sublime.Region(region.end(), line.end())
+					textRightToCursor = self.view.substr(regionRightToCursor)
+					self.view.erase(edit, regionRightToCursor)
+
 					matchObj = re.match(r'(\s*)(\w*).*$', string, re.M)
 					whitespaces = matchObj.group(1)
 					#print ("Whitespace found: '" + whitespaces + "'")
-					newLines = ' {\n'+whitespaces+'\t\n'+whitespaces+'}'
+					newLines = ' {\n'+whitespaces+'\t'+textRightToCursor+'\n'+whitespaces+'}'
 
 					# Append: " {LINEBREAK TABS TAB LINEBREAK TABS}"
-					self.view.insert(edit, line.end(), newLines)
+					self.view.insert(edit, region.end(), newLines)
 
 					# Refresh region since we just shifted its coordinates with the previous command
 					region = self.view.sel()[counter]
 
 					# Cursor re-positioning after second LINEBREAK
 					pos = region.begin()
-					pos = pos - len(whitespaces) - 2
+					pos = pos - len(whitespaces) - len(textRightToCursor) - 2
 					self.view.sel().subtract(region)
 					self.view.sel().add(pos)
